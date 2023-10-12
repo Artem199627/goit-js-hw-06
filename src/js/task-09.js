8833a7d94faad46dcf46159c74b3092cc70d456d
Artem199627 <artemishko@gmail.com>
2023-10-12 15:51:58 +0200

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const span = document.querySelector("span.color");
+const btnChangeColor = document.querySelector("button.change-color");
+btnChangeColor.addEventListener("click", changeColor);
+
+function changeColor() {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;  
+}
+
