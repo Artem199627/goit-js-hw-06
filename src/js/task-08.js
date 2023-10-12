65e6321bdd7f20c42782c1e026c0fa27dedcc2af
Artem199627 <artemishko@gmail.com>
2023-10-12 15:33:49 +0200

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
+console.log(loginForm);
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const login = form.elements.login.value;
+    const password = form.elements.password.value;
+    if (login === "" || password === "") {
+        return console.log("Please fill in all the fields!");
+    }
+    console.log(`Login: ${login.value}, Password: ${password.value}`);
+    form.reset();
+}
 
