9446ed3f886435f3b76e1e514cefdf7d99e9e5f3
Artem199627 <artemishko@gmail.com>
2023-10-13 16:44:58 +0200

    task6

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,11 @@
 const input = document.querySelector("#validation-input");
-input.addEventListener("blur", onInputBlur);
-function onInputBlur() {
-    if (input.value.length === Number(input.dataset.length)) {
-        input.classList.remove("invalid");
-        input.classList.add("valid");
+const length = input.dataset.length;
+input.addEventListener("blur", ()  => {
+    if (input.value.length !== Number(length)) { 
+        input.classList.add("invalid");
+        input.classList.remove("valid");
     } else {  
         input.classList.remove("invalid");
         input.classList.add("valid");
     }
-}
+});
